840d0ab9b7e191d06f53d0b31294d8c991d9ca75
Bobakanoosh55555 <klynd@rocketmail.com>
2025-04-01 15:23:11 -0400

    Update leaderboard.js

--- slippi/leaderboard.js
+++ slippi/leaderboard.js
@@ -191,7 +191,7 @@ document.addEventListener('DOMContentLoaded', function() {
     playerCardsContainer.innerHTML = "";
     const minRating = 700, maxRating = 3000;
     const viewHeight = altView.offsetHeight;  // expected 8000px
-    const verticalOffset = 110;  // new constant to shift all cards/lines down by 200px
+    const verticalOffset = 130;  // new constant to shift all cards/lines down by 200px
     
     const altRect = altView.getBoundingClientRect();
     const barRect = verticalBar.getBoundingClientRect();
